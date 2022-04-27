ce2bc0cba9987a13207372cdec922117c84a6fd2
kiovahnleon <al20760618@ite.edu.mx>
2022-04-26 23:38:51 -0700

    biografia2

--- topicosAvanzados/Biografia2/biografia/src/App.jsx
+++ topicosAvanzados/Biografia2/biografia/src/App.jsx
@@ -2,21 +2,33 @@ function App() {
 
   return (
     <div className="text-slate-700 mx-14 ">
-      <h1 className=" md:text-6xl text-center font-black rounded-lg uppercase text-2xl p-10 text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-orange-600">Biografia</h1>
-      <div className=" text-justify bg-white rounded-lg p-[20%] ">
-        <p>Nací en Oaxaca, el 22 de Marzo del año 2000.
-          Recibí mi educación preescolar en un pueblo en Oaxaca.
-        </p><br />
-
-        <p>En 2006, mi madre, mi padre, mi hermana y yo nos mudamos a Ensenada, Baja California.
-          Inicié la escuela primaria en la primaria "Las Américas" y la terminé en la primaria "Soledad Anaya Solórzano", que era la misma escuela, pero en el turno matutino.
-          Asistí a la "Secundaria Técnica 19". Luego asistí al bachillerato "CBTis 41".
-        </p><br />
-
-        <p>
-          En Septiembre de 2018, viajé a Haifa, Israel para servir en el  Centro Mundial Bahai como jardinero. La mitad de mi servicio fue en los jardines en Haifa, y la otra mitad de mi servicio estuve sirviendo en los jardines en Akka. Estuve sirviendo por un año y medio. En Marzo de 2020 regresé a México.
-          Actualmente estoy cursando cuarto semestre de Ing. en Sistemas Computacionales en el TECNM campus Ensenada.
-        </p><br />
+      <h1 className="text-2xl md:text-6xl text-center font-black rounded-lg uppercase p-10 text-slate-100">Biografia</h1>
+      <div className=" bg-white rounded-lg p-[10%] ">
+        <div className="pb-10 grid grid-cols-2 gap-4">
+          <img src="https://pbs.twimg.com/media/FRVHj_VVIAAFCFa?format=png&name=240x240" alt="profilepic" className="rounded-full" />
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-orange-600"><span className="md:text-6xl text-2xl font-bold">Kiovahn Leon</span><br /> <span className="font-thin text-lg">Instituto Tecnologico de Ensenada</span></h1>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="..."><h1 className=" font-black">Experiencia</h1>Experiencia muy limitada</div>
+          <div className="..."><h1 className="font-black">Habilidades Personales</h1>
+            <ul>
+              <li>Moverle un poco a la guitarra</li>
+              <li>Jugar medianamente futbol</li>
+            </ul>
+          </div>
+          <div className="..."><h1 className='font-black'>Educacion</h1>
+            <ul>
+              <li>EST Num.19 - Tecnico en Electronica</li>
+              <li>CBTis Num. 41 - Tecnico en Electronica</li>
+            </ul>
+          </div>
+          <div className="..."><h1 className='font-black'>Referencias</h1>
+            <ul>
+              <li>Pablo Palmeras <span className='font-light'>(465 929 89 12)</span></li>
+              <li>Kevin Salaas <span className='font-light'>(475 858 90 14)</span></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
